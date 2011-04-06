1e5ad855fcba5d646b873d12ca8144d3fb28a8ed
Anda Bereczky <anda@bereczky.me>
2011-04-06 15:26:42 -0500

    Enabled using the MySQL database. Implemented the initial model.

--- apartmenthunt/models.py
+++ apartmenthunt/models.py
@@ -1,3 +1,19 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+class CraigsListSite(models.Model):
+	''' A class representing the apartments website of a craigslist subdomain.'''
+	site_name = models.CharField(max_length=64)
+	site_subdomain = models.CharField(max_length=64)
+	last_collection_date = models.DateTimeField('last collection date')
+	def __unicode__(self):
+		return self.site_name;
+	def was_collected_today(self):
+		return self.last_collection_date.date() == datetime.date.today()
+
+class Apartment(models.Model):
+	''' A class representing an ad for an apartment.'''
+	craigs_list_site = models.ForeignKey(CraigsListSite)
+	full_ad_text = models.TextField()
+	def __unicode__(self):
+		return self.full_ad_text;
--- settings.py
+++ settings.py
@@ -4,17 +4,17 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    # ('Anda Bereczky', 'anda@bereczky.me'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'mysql',               # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'aphunt',                # Or path to database file if using sqlite3.
+        'USER': 'aphuntadmin',           # Not used with sqlite3.
+        'PASSWORD': 'uXtAAUbXtbXqXJ9U',  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -89,6 +89,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+	'apartmenthunt',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
